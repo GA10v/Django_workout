1d806e579aa0224a6e2641097a180b29784da698
Galovach Andrey <a.galovach@yandex.ru>
2022-05-24 04:36:42 +0300

    fixed

---
+++ fit/fit/apps/workout/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Workout(models.Model):
+    name = models.CharField('name', max_length=100)
+    focus = models.CharField('focus', max_length=50)
+    types = models.CharField('type', max_length=50)
+    difficult = models.CharField('difficult', max_length=20)
+    equirment = models.CharField('equirement', max_length=50)
+    icon = models.CharField('img intro', max_length=200)
+    img = models.CharField('img link', max_length=200)
+    muscles = models.CharField('img muscles', max_length=200)
--- fit/fit/settings.py
+++ fit/fit/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+from . import config
 
 from pathlib import Path
 import sys, os
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g2_0x@v0nkne%5jo_$izua&xaklnndvd)2jqi1&5osfbd9yeqz'
+SECRET_KEY = config.SECRET_KEY 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'workout.apps.WorkoutConfig'
 ]
 
 MIDDLEWARE = [

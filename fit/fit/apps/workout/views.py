661d9b17a46952cb8ea74c7559a8f50d6eacd173
Galovach Andrey <a.galovach@yandex.ru>
2022-05-25 15:08:43 +0300

    models, views

--- fit/fit/apps/workout/views.py
+++ fit/fit/apps/workout/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Workout
 
 def workout(request):
-    return HttpResponse('workouts')
+    first_workout = Workout.objects.all()[1]
+    workouts = Workout.objects.all()[2:7]
+    return render(request, 'workout/index.html', {'first_workout':first_workout,'workouts':workouts, 'title':'title'})
 
